661f70bc007c2ffa71f11cff9d1c70ed8893fc4c
syu <yuji1113@live.jp>
2020-04-01 15:00:41 +0900

    Load images with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
           },
         ]
       },
+      {
+        test: /\.(png|jpe?g|svg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: 'images/[name].[ext]'
+            }
+          },
+        ]
+      },
     ]
   },
   plugins: [
