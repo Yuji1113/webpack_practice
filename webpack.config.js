7c4f3d6ed41deb93e56835593d9c4efbc4192e10
syu <yuji1113@live.jp>
2020-04-02 11:11:06 +0900

    Install Postcss and Plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+// 'production' か 'development' を指定
+const MODE = "development";
+// ソースマップの利用有無(productionのときはソースマップを利用しない)
+const enabledSourceMap = MODE === "development";
+
+
 const path = require('path')
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
@@ -20,9 +27,57 @@ module.exports = {
           },
           {
             loader: 'css-loader',
+            options: {
+              // オプションでCSS内のurl()メソッドを取り込む
+              // url: true,
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            }
+          },
+          // PostCSSのための設定
+          {
+            loader: "postcss-loader",
+            options: {
+              // PostCSS側でもソースマップを有効にする
+              sourceMap: enabledSourceMap,
+
+              plugins: [
+                //CSS　圧縮
+                // require("cssnano")(),
+
+                // hexrgba
+                require("postcss-hexrgba"),
+
+                // CSS Sotringを有効化
+                // CSSを任意に並べる
+                require("postcss-sorting")({
+                  "properties-order": [
+                    "margin",
+                    "padding",
+                    "border",
+                    "background"
+                  ]
+                }),
+                // Autoprefixerを有効化
+                // ベンダープレフィックスを自動付与する
+                require("autoprefixer")({
+                  grid: true
+                }),
+
+              ]
+            }
           },
           {
             loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap
+            }
           },
         ]
       },
